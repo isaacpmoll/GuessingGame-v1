6f6c6460b835cd97335fd5b8e13e1779b4b7add6
isaacpmoll <73903177+isaacpmoll@users.noreply.github.com>
2021-01-20 12:34:47 -0500

    Update GuessingGame-v1.py

--- GuessingGame-v1.py
+++ GuessingGame-v1.py
@@ -1,1 +1,19 @@
 #Do Stuff Here
+
+
+import time
+import random
+
+name = input("What is your name? ")
+print("Well then, " +name+ ", let's play a guessing game!")
+time.sleep(1)
+mi = int(input("Pick the minimum range: "))
+ma = int(input("Pick the maximum range: "))
+ran = random.randint(mi, ma)
+
+guess = int(input("What is your guess? "))
+
+if guess==ran:
+	print("correct!")
+else:
+	print("wrong...")
